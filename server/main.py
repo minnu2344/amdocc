889734a600c6f67abf5cde223d786e4b0e447a6b
Harshit Manchanda <128061019+harshit6239@users.noreply.github.com>
2025-02-03 21:22:35 +0530

    changes

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+import uvicorn
 from server.models.generation import generate_explanation
 
 app = FastAPI()
@@ -13,3 +14,8 @@ async def classifyText(request: Request):
     text = data.get("text", "")
     original_statements, truth_values, explanations, sources_list = generate_explanation(text)
     return {"original_statements": original_statements, "truth_values": truth_values, "explanations": explanations, "sources_list": sources_list}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Use the PORT variable provided by Render
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    
